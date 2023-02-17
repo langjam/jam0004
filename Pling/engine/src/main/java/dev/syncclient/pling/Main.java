51560583ceb9b173f2a781ca2daa5bb2e007c82d
Christian Bergschneider <bergschneider@stellarverse.de>
2023-02-18 00:09:15 +0100

    feat: lexer

--- Pling/engine/src/main/java/dev/syncclient/pling/Main.java
+++ Pling/engine/src/main/java/dev/syncclient/pling/Main.java
@@ -1,7 +1,39 @@
 package dev.syncclient.pling;
 
+import dev.syncclient.pling.lexer.Lexer;
+import dev.syncclient.pling.lexer.Token;
+import dev.syncclient.pling.utils.StringUtils;
+
+import java.io.File;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        StringBuilder source = new StringBuilder();
+        File file = new File("examples/test_lex.pling");
+        if (file.exists()) {
+            try {
+                Scanner scanner = new Scanner(file);
+                while (scanner.hasNextLine()) {
+                    source.append(scanner.nextLine()).append("\n");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        Lexer lexer = new Lexer();
+        List<Token.AbstractToken> tokenList = lexer.lex(source.toString());
+
+        System.out.println("=== BEGIN Tokens ===");
+        for (Token.AbstractToken token : tokenList) {
+            System.out.println(StringUtils.ljust(token.getType().toString(), 10) + ": " + token.getValue());
+        }
+        System.out.println("=== STOP  Tokens ===");
+
     }
 }
--- Pling/engine/src/main/java/dev/syncclient/pling/lexer/Lexer.java
+++ Pling/engine/src/main/java/dev/syncclient/pling/lexer/Lexer.java
@@ -1,10 +1,110 @@
 package dev.syncclient.pling.lexer;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class Lexer {
-    public List<Token> lex(String source) {
-        System.out.println("Hello world!");
-        return null;
+    private String source;
+    private final List<Token.AbstractToken> tokens = new ArrayList<>();
+    private int current = 0;
+
+    private char next() {
+        current++;
+        return source.charAt(current - 1);
+    }
+
+    private char peek() {
+        if (current >= source.length()) {
+            return ' ';
+        }
+
+        return source.charAt(current);
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    private String tryEat(Function<Character, Boolean> predicate, boolean silentEOF) {
+        StringBuilder builder = new StringBuilder();
+        while (predicate.apply(peek())) {
+            if (isAtEnd()) {
+                if (silentEOF) {
+                    return builder.toString();
+                }
+                throw new EOFException();
+            }
+            builder.append(next());
+        }
+
+        return builder.toString();
+    }
+
+    private String tryEat(Function<Character, Boolean> predicate) {
+        return tryEat(predicate, false);
+    }
+
+    public List<Token.AbstractToken> lex(String source) {
+        this.source = source;
+        tokens.clear();
+
+        while (!isAtEnd()) {
+            char c = peek();
+
+            if (Character.isWhitespace(c)) {
+                next();
+            } else if (c == '=') {
+                tokens.add(Token.ASSIGN.createToken(next() + ""));
+            } else if (c == '#') {
+                tokens.add(Token.REFERENCE.createToken(next() + ""));
+            } else if (c == '[') {
+                tokens.add(Token.OPEN.createToken(next() + ""));
+            } else if (c == ']') {
+                tokens.add(Token.CLOSE.createToken(next() + ""));
+            } else if (c == ',') {
+                tokens.add(Token.COMMA.createToken(next() + ""));
+            } else if (c == '.') {
+                tokens.add(Token.DOT.createToken(next() + ""));
+            } else if (c == ';') {
+                tokens.add(Token.END.createToken(next() + ""));
+            } else if (c == '`') {
+                next();
+                lastWasEscape = false;
+                tokens.add(Token.STRING.createToken(tryEat(Lexer::tryEatString)));
+                next();
+            } else if (c == '/') {
+                next();
+                if (peek() == '/') {
+                    next();
+                    tryEat((character) -> character != '\n', true);
+                } else {
+                    throw new LexicalException("Unexpected character: " + c);
+                }
+            } else if (Character.isDigit(c)) {
+                tokens.add(Token.NUMBER.createToken(tryEat(Character::isDigit)));
+            } else if (Character.isAlphabetic(c)) {
+                tokens.add(Token.IDENTIFIER.createToken(tryEat((character) -> Character.isAlphabetic(character) || Character.isDigit(character))));
+            } else {
+                throw new LexicalException("Unexpected character: " + c);
+            }
+        }
+
+        return tokens;
+    }
+
+    private static boolean lastWasEscape = false;
+    private static Boolean tryEatString(Character character) {
+        if (lastWasEscape) {
+            lastWasEscape = false;
+            return true;
+        }
+
+        if (character == '\\') {
+            lastWasEscape = true;
+            return true;
+        }
+
+        return character != '`';
     }
 }
