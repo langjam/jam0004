91c1da4f4a364a46e34e20bd8c67fe30d1bd62c2
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-02-18 08:53:19 -0500

    scanner

---
+++ catch-23/src/lexer.c
@@ -0,0 +1,29 @@
+#include "tokens.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+char* scan(const char* source_code) {
+    
+    FILE* file = fopen(source_code, "r");
+    char* result;
+    
+    if (file == NULL) *result = -1;
+    
+    else {
+        
+        fseek(file, 0, SEEK_END);
+        
+        long size = ftell(file);
+        char* result = malloc(size);
+        
+        fseek(file, 0, SEEK_SET);
+        
+        if (result) 
+            fread(result, 1, size, file);
+        
+        fclose(file);
+    }
+    
+    return result;
+}
+//token* tokenize(const char* source_code);
