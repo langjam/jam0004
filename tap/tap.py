7b240115223103f4b3aa3f2b2bec1c9e5ff18380
Cerabbite <98265679+Cerabbite@users.noreply.github.com>
2023-02-19 10:15:35 +0100

    Bug: Colon is only detected if " " before, and EOF is never detected

--- tap/tap.py
+++ tap/tap.py
@@ -1,190 +1,195 @@
 from enum import Enum
+from dataclasses import dataclass
+from typing import Optional
 
 
+# Show the line on which the error happens and then point with arrows
+# where the arrow is
+# This won't be super accurate as it will always only point out the first
+# character of the error, but it is a good start
 class Error:
-    def __init__(self, error_type, message):
-        self.error_type = error_type
-        self.message = message
+    def __init__(self, position: tuple[str, int, int], error_name, details):
+        self.position: tuple[str, int, int] = position
+        self.error_name = error_name
+        self.details = details
+
+    def show(self):
+        error = f"{self.position[0]} {self.position[1]}:{self.position[2]}\n"
+        error += f"{self.error_name}: {self.details}\n"
+        return error
 
 
-class SomeError(Error):
-    pass
+class IllegalCharacterError(Error):
+    def __init__(self, position: tuple[str, int, int], details):
+        super().__init__(position, "Illegal character error", details)
+
+
+class UnkownKeywordError(Error):
+    def __init__(self, position: tuple[str, int, int], details):
+        super().__init__(position, "Unknown keyword error", details)
+
+
+class Keyword:
+    keywords = ["thumb",
+                "index",
+                "middle",
+                "ring",
+                "pinky",
+                "soft",
+                "medium",
+                "loud"
+                ]
 
 
 class TokenType(Enum):
-    # STR = "STR"
     INT = "INT"
     FLOAT = "FLOAT"
-    # THUMB = "THUMB"
-    # INDEX = "INDEX"
-    # MIDDLE = "MIDDLE"
-    # RING = "RING"
-    # PINKY = "PINKY"
-    # SOFT = "SOFT"
-    # MEDIUM = "MEDIUM"
-    # LOUD = "LOUD"
-    # COLON = "COLON"
-    PLUS = "PLUS"
-    MINUS = "MINUS"
-    MULTIPLY = "MULTIPLY"
-    DIVIDE = "DIVIDE"
-    LPAREN = "LPAREN"
-    RPAREN = "RPAREN"
+    KEYWORD = "KEYWORD"
+    COLON = ":"
+    EOF = "EOF"
 
 
+@dataclass
 class Token:
-    def __init__(self, type_: TokenType, value=None):
-        self.type = type_
-        self.value = value
-
-    def __repr__(self) -> str:
-        if self.value:
-            return f"{self.type.value}:{self.value}"
-        else:
-            return f"{self.type.value}"
+    Type: TokenType
+    Value: Optional[str] = None
 
 
 class Lexer:
-    def __init__(self, filename: str, text: str):
-        self.filename = filename
-        self.text = text
-        self.line_number = 1
-        self.cursor = -1
-        self.current_char = None
+    def __init__(self, filename: str, code: str):
+        self.filename: str = filename
+        self.code: str = code
+        self.column: int = -1
+        self.prev_column: int = self.column
+        self.line_number: int = 1
+        self.cursor: int = -1
+        self.current_char: Optional[str] = None
 
         self.advance()
 
-    def advance(self) -> None:
+    def advance(self):
         self.cursor += 1
-        self.current_char = self.text[self.cursor] if \
-            self.cursor < len(self.text) else None
+        self.column += 1
+        if self.cursor < len(self.code):
+            self.current_char = self.code[self.cursor]
+            print(self.current_char, "Hello")
+            if self.current_char == "\n":
+                self.prev_column = self.column
+                self.column = 0
+                self.line_number += 1
+                self.advance()
+        else:
+            self.current_char = None
 
-        if self.current_char == "\n":
-            self.line_number += 1
-            self.advance()
+    def previous(self):
+        self.cursor -= 1
+        self.column -= 1
 
-    def tokenize(self) -> tuple[list[Token], None]:
-        tokens: list[Token] = []
+        if self.cursor >= 0:
+            self.current_char = self.code[self.cursor]
+            if self.current_char == self.column:
+                if self.prev_column is None:
+                    print("Warn: column cannot be reset")
+                else:
+                    self.column = self.prev_column
+                self.previous()
+
+    def tokenize(self):
+        tokens = []
 
         while self.current_char is not None:
+            if self.current_char not in " \t\n":
+                print(self.current_char)
+
             if self.current_char.isdigit():
-                tokens.append(self.number())
-            elif self.current_char == "+":
-                tokens.append(Token(TokenType.PLUS))
+                ret_value = self.number()
+                if isinstance(ret_value, Error):
+                    return ret_value.show()
+                tokens.append(ret_value)
+                if self.current_char == "\n":
+                    tokens.append(Token(TokenType.EOF))
                 self.advance()
-            elif self.current_char == "-":
-                tokens.append(Token(TokenType.MINUS))
+            elif self.current_char.isalpha():
+                ret_value = self.identify()
+                if isinstance(ret_value, Error):
+                    return ret_value.show()
+                tokens.append(ret_value)
+                print(f"Ret value: {self.current_char}")
                 self.advance()
-            elif self.current_char == "*":
-                tokens.append(Token(TokenType.MULTIPLY))
+            elif self.current_char == ":":
+                tokens.append(Token(TokenType.COLON))
                 self.advance()
-            elif self.current_char == "/":
-                tokens.append(Token(TokenType.DIVIDE))
-                self.advance()
-            elif self.current_char == "(":
-                tokens.append(Token(TokenType.LPAREN))
-                self.advance()
-            elif self.current_char == ")":
-                tokens.append(Token(TokenType.RPAREN))
-                self.advance()
-            else:
-                print("Illegal character")
+            elif self.current_char in "\n":
+                tokens.append(Token(TokenType.EOF))
+                print("Hello")
                 self.advance()
 
-        return tokens, None
+        return tokens
 
     def number(self):
         number_str = ""
         dot_found = False
 
-        while True:
-            if self.current_char is None:
-                break
-
+        while self.current_char is not None:
             if self.current_char == ".":
                 match dot_found:
+                    case True:
+                        return IllegalCharacterError((self.filename,
+                                                      self.line_number,
+                                                      self.column),
+                                                     "'.'")
                     case False:
                         dot_found = True
                         number_str += "."
-                    case True:
-                        print("Error: To many dots")
-                        return "Loud Middlefinger tap: Error msg"
-            elif self.current_char in " \t":
-                break
-            elif not self.current_char.isdigit() and self.current_char != "_":
+            elif self.current_char.isdigit():
+                number_str += self.current_char
+            elif self.current_char in " \t\n":
                 break
             else:
-                number_str += self.current_char
+                return IllegalCharacterError((self.filename,
+                                              self.line_number,
+                                              self.column),
+                                             f"'{self.current_char}'")
 
             self.advance()
 
+        # print(number_str, dot_found)
         if dot_found:
             return Token(TokenType.FLOAT, float(number_str))
         else:
             return Token(TokenType.INT, int(number_str))
 
-    def loudness(self):
-        loudness_str = ""
-        space_found = False
+    def identify(self):
+        curr_str = ""
 
-        print(self.current_char)
-        while True:
-            if self.current_char is None:
-                break
+        while self.current_char is not None:
+            if curr_str in Keyword.keywords:
+                print('Inside')
+                # self.previous()
+                return Token(TokenType.KEYWORD, curr_str)
+            elif self.current_char in " \t\n":
+                print(self.current_char)
+                return UnkownKeywordError((self.filename,
+                                           self.line_number,
+                                           self.column),
+                                          f"'{curr_str}'")
+            else:
+                print("1")
+                curr_str += self.current_char
+                self.advance()
 
-            # if self.current_char
-            self.advance()
-
-
-class NumberNode:
-    def __init__(self, token):
-        self.token = token
-
-    def __repr__(self):
-        return f"{self.token}"
-
-
-class BinaryOperatorNode:
-    def __init__(self, left_node, operator_token, right_node):
-        self.left_node = left_node
-        self.operator_token = operator_token
-        self.right_node = right_node
-
-    def __repr__(self):
-        return f"({self.left_node}, {self.operator_token}, {self.right_node})"
-
-
-class Parser:
-    def __init__(self, tokens):
-        self.tokens = tokens
-        self.token_index = -1
-        self.current_token = None
-
-        self.advance()
-
-    def advance(self):
-        self.token_index += 1
-        if self.token_index < len(self.tokens):
-            self.current_token = self.tokens[self.token_index]
-
-        return self.current_token
-
-    def parse(self):
-        result = self.expr()
-        return result
-
-    def factor(self):
-        token = self.current_token
-
-        if token.type == TokenType.INT or \
-           token.type == TokenType.FLOAT:
-            self.advance()
-            return NumberNode()
-
-    def term(self):
-        return self.binary
+        if curr_str in Keyword.keywords:
+            print("Outside")
+            # self.previous()
+            return Token(TokenType.KEYWORD, curr_str)
+        else:
+            return UnkownKeywordError((self.filename,
+                                       self.line_number,
+                                       self.column),
+                                      f"'{curr_str}'")
 
 
-simple_program = "7+9*2/(9+2)"
+simple_program = "thumb :\n"
+print(simple_program)
 lexer = Lexer("none", simple_program)
 print(lexer.tokenize())
